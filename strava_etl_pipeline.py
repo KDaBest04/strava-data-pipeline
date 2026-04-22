cc8703d2e720569d08a19c3dbdb115e448a77f95
Dao Vu Khanh <126807347+KDaBest04@users.noreply.github.com>
2026-04-22 22:08:42 +0700

    Modify data extraction and transformation logic
    Updated the number of activities fetched from Strava API and ensured proper handling of data transformations.

--- strava_etl_pipeline.py
+++ strava_etl_pipeline.py
@@ -30,7 +30,7 @@ def extract_latest_data(access_token):
     print("Checking for new run")
     url = "https://www.strava.com/api/v3/athlete/activities"
     headers = {"Authorization": f"Bearer {access_token}"}
-    params = {"per_page": 200, "page": 1}
+    params = {"per_page": 10, "page": 1}
 
     response = requests.get(url, headers=headers, params=params)
     return pd.DataFrame(response.json())
